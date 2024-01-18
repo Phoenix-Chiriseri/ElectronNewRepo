c12807fc9db358b9b77e18d9d4aad0cbe782e505
itai chiriseri <itaineilchiriseri@gmail.com>
2024-01-18 15:29:28 +0200

    reenterd the cdns

--- ElectronPos/resources/views/pages/cart/index.blade.php
+++ ElectronPos/resources/views/pages/cart/index.blade.php
@@ -1,7 +1,7 @@
-<script src="{{ asset('assets') }}/css/jquery-3.3.1.min.js"></script>
-<script src="{{ asset('assets') }}/js/axios.min.js"></script>
-<link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <script>
     $(document).ready(function () {
