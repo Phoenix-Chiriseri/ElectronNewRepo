384031bfe2733114c0c66c26556c77a7308e35f6
itai chiriseri <itaineilchiriseri@gmail.com>
2024-01-18 15:23:47 +0200

    added the cdns locally

--- ElectronPos/resources/views/pages/cart/index.blade.php
+++ ElectronPos/resources/views/pages/cart/index.blade.php
@@ -1,5 +1,6 @@
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script src="{{ asset('assets') }}/css/jquery-3.3.1.min.js"></script>
+<script src="{{ asset('assets') }}/js/axios.min.js"></script>
+<link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <script>
