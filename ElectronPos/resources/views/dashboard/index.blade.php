2d6248296883edbd8d8327fe509d2a5b4d477b57
itai chiriseri <itaineilchiriseri@gmail.com>
2024-01-24 18:32:17 +0200

    removed the redundant text from the dashboard

--- ElectronPos/resources/views/dashboard/index.blade.php
+++ ElectronPos/resources/views/dashboard/index.blade.php
@@ -198,15 +198,15 @@
         <div class="row">
         <div class="col-md-6 mb-md-0 mb-4">
         <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
-        <img class="w-10 me-3 mb-0" src="../assets/img/logos/mastercard.png" alt="logo">
-        <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;7852</h6>
+        <img class="w-10 me-3 mb-0">
+        <h6 class="mb-0"></h6>
         <i class="material-icons ms-auto text-dark cursor-pointer" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Card">edit</i>
         </div>
         </div>
         <div class="col-md-6">
         <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
-        <img class="w-10 me-3 mb-0" src="../assets/img/logos/visa.png" alt="logo">
-        <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;5248</h6>
+        <img class="w-10 me-3 mb-0">
+        <h6 class="mb-0"></h6>
         <i class="material-icons ms-auto text-dark cursor-pointer" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Card">edit</i>
         </div>
         </div>
