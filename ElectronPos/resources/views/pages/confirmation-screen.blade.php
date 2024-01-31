6eea3df58371b0395162263249bc5c2337bd1a6c
itai chiriseri <itaineilchiriseri@gmail.com>
2024-01-31 17:57:10 +0200

    made slight change on the selling screen

--- ElectronPos/resources/views/pages/confirmation-screen.blade.php
+++ ElectronPos/resources/views/pages/confirmation-screen.blade.php
@@ -49,7 +49,7 @@
                     <div class="card my-4">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                                <h6 class="text-white text-capitalize ps-3">Actions</h6>
+                                <h6 class="text-white text-capitalize ps-3">How would you like the customer to receive payment</h6>
                             </div>
                         </div>
                         <div class="card-body px-3 pb-2">
