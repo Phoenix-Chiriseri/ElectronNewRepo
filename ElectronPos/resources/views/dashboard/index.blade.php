80bf97bc72773faf061df96656e0bc0c53cdfc17
itai chiriseri <itaineilchiriseri@gmail.com>
2024-02-01 16:22:16 +0200

    changed the titles to dashboard only

--- ElectronPos/resources/views/dashboard/index.blade.php
+++ ElectronPos/resources/views/dashboard/index.blade.php
@@ -7,23 +7,23 @@
         <nav aria-label="breadcrumb">
         <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
         <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Billing</li>
+        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
         </ol>
-        <h6 class="font-weight-bolder mb-0">Billing</h6>
+        <h6 class="font-weight-bolder mb-0">Dashboard</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
         <div class="ms-md-auto pe-md-3 d-flex align-items-center">
         <div class="input-group input-group-outline">
-        <label class="form-label">Type here...</label>
-        <input type="text" class="form-control">
+        
+       
         </div>
         </div>
         <ul class="navbar-nav  justify-content-end">
         <li class="nav-item d-flex align-items-center">
-        <a class="btn btn-outline-primary btn-sm mb-0 me-3" target="_blank" href="https://www.creative-tim.com/builder?ref=navbar-material-dashboard">Online Builder</a>
+       
         </li>
         <li class="mt-2">
-        <a class="github-button" href="https://github.com/creativetimofficial/material-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/material-dashboard on GitHub">Star</a>
+      
         </li>
         <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
         <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
@@ -41,7 +41,7 @@
         </li>
         <li class="nav-item dropdown pe-2 d-flex align-items-center">
         <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-        <i class="fa fa-bell cursor-pointer"></i>
+        
         </a>
         <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
         <li class="mb-2">
@@ -113,9 +113,6 @@
         </ul>
         </li>
         <li class="nav-item d-flex align-items-center">
-        <a href="../pages/sign-in.html" class="nav-link text-body font-weight-bold px-0">
-        <i class="fa fa-user me-sm-1"></i>
-        <span class="d-sm-inline d-none">Sign In</span>
         </a>
         </li>
         </ul>
