179d4d05d2fe60fd2ac2ded2869f6831d01cd614
itai chiriseri <itaineilchiriseri@gmail.com>
2024-02-01 13:50:29 +0200

    added the products table

--- ElectronPos/resources/views/pages/view-products.blade.php
+++ ElectronPos/resources/views/pages/view-products.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.debug.js"></script>
 <x-layout bodyClass="g-sidenav-show  bg-gray-200">
     <x-navbars.sidebar activePage="tables"></x-navbars.sidebar>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
@@ -7,24 +9,43 @@
         @if(!$products)
         <h1>No Products Found</h1>
         @endif
+        <script>
+           function generatePDF() {
+    var doc = new jsPDF();
+
+    // Title
+    doc.setFontSize(12);
+    doc.text('Available Products', 10, 10);
+
+    // Table
+    var htmlContent = document.getElementById('productsTable').innerHTML;
+    doc.fromHTML(htmlContent, 10, 20, {
+        width: 180
+    });
+
+    // Save the PDF
+    doc.save('AvailableProducts.pdf');
+}
+            </script>
         <div class="container-fluid py-4">
             <div class="row">
                 <div class="col-12">
-                    <div class="card my-4">
+                    <div class="card my-4" id="productsTable">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
                                 <h6 class="text-white text-capitalize ps-3">Products</h6>
                                 <h6 class="text-white text-capitalize ps-3">Number Of Products {{$productCount}}</h6>
                             </div>
                             <hr>
+                            <button onclick="generatePDF()" class = "btn btn-info"><i class = "fa fa-print"></i>Generate PDF</button>
                             <a class="btn btn-danger" href="{{ route('create-product') }}"
                                         role="tab" aria-selected="true">
                                         <i class="material-icons text-lg position-relative"></i>
                                         <span class="ms-1">Add New Product</span>
                             </a>
                         </div>
-                        <div class="card-body px-0 pb-2">
-                            <div class="table-responsive p-0">
+                        <div class="card-body px-0 pb-2" i>
+                            <div class="table-responsive p-0" d="productsTable" >
                                 <table class="table align-items-center mb-0">
                                     <thead>
                                         <tr>
