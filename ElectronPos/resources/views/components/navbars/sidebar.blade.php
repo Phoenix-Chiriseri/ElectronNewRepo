14d62af2ab2414b6493b3fcc2040cc59a8f6f29b
itai chiriseri <itaineilchiriseri@gmail.com>
2024-02-08 16:48:57 +0200

    removed the users tab

--- ElectronPos/resources/views/components/navbars/sidebar.blade.php
+++ ElectronPos/resources/views/components/navbars/sidebar.blade.php
@@ -140,15 +140,7 @@
                     <a class="dropdown-item" href="{{ route('access-rights') }}">Access Rights</a>
                 </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link text-white"
-                    href="{{ route('view-reports') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1.2rem;" class="fas fa-book ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Reports</span>
-                </a>
-            </li>
+           
             <li class="nav-item dropdown">
                 <a class="nav-link btn btn-dark btn-sm dropdown-toggle text-right" href="#" id="inventoryDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i style="font-size: 1.2rem;" class="fa fa-cogs me-2 ps-2 pe-2 text-center"></i>
