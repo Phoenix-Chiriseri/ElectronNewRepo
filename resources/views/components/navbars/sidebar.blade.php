da2a323b19d099e6420384f87689048d2e9f8c33
itai chiriseri <itai.neil@everythingzimbabwean.com>
2025-08-14 17:55:15 +0200

    removed the inventory valuation report from the navigation bar

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -106,7 +106,6 @@
                     <a class="dropdown-item" href="{{ route('grv-enquiry') }}">GRV Enquiry</a>
                     <a class="dropdown-item" href="{{ route('create-purchaseorder') }}">Purchase Orders</a>
                     <a class="dropdown-item" href="{{ route('stock-enquiry') }}">Stock Enquiry</a>
-                    <a class="dropdown-item" href="{{ route('view-inventoryvaluation') }}">Inventory Valuation Report</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
