53287dab8e5c9612ac7eb1a5488b1ecfdba0abeb
itai chiriseri <itai.neil@everythingzimbabwean.com>
2025-08-21 17:07:00 +0200

    removed the configure printers on the sidebar

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -148,15 +148,6 @@
                     <a class="dropdown-item" href="{{ route('reports') }}">View Reports</a>
                 </div>
             </li>
-             <li class="nav-item">
-                <a class="nav-link text-white"
-                href="{{ route('configure-printers') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1.2rem;" class="fa-solid fa-print ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Configure Printers</span>
-                </a>
-            </li> 
             <li class="nav-item">
                 <a class="nav-link text-white"
                 href="{{ route('view-payment-types') }}">
