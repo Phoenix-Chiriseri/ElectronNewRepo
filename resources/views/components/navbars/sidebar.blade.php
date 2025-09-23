0d31e7b2194350d1c2f1282fa71ef7cf8ce78b8c
itai chiriseri <itai.neil@everythingzimbabwean.com>
2025-09-23 16:53:11 +0200

    restored the cart nav

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -129,7 +129,7 @@
                     <span class="nav-link-text ms-1">Invoices</span>
                 </a>
             </li>
-            <!-- <li class="nav-item">
+            <li class="nav-item">
                 <a class="nav-link text-white"
                     href="cart">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -137,7 +137,7 @@
                     </div>
                     <span class="nav-link-text ms-1">Cart</span>
                 </a>
-            </li> -->
+            </li>
             <li class="nav-item dropdown">
                 <a class="nav-link btn btn-dark btn-sm dropdown-toggle text-right" href="#" id="inventoryDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i style="font-size: 1.2rem;" class="fas fa-users me-2 ps-2 pe-2 text-center"></i>
